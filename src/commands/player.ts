9bc129bc8ad54c46d524f59b0f6403b77e45fafb
GregoryGDT <gregorygdt464@gmail.com>
2023-08-07 21:13:47 -0300

    Adding author and title in playembed

--- src/commands/player.ts
+++ src/commands/player.ts
@@ -2,9 +2,12 @@ import { play } from "./player/play"
 import { addCommand, Command } from "../utils/commands"
 import { skip } from "./player/skip"
 import { pause } from "./player/pause"
+import { resume } from "./player/resume"
+
 
 export const playerCommandsList: Array<Command> = [
     addCommand("play", play),
     addCommand("skip", skip),
     addCommand("pause", pause),
+    addCommand("resume", resume)
 ]
--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,27 +1,54 @@
 import { EmbedBuilder, RGBTuple } from '@discordjs/builders'
 import { Message } from 'discord.js'
+import { APIEmbedField } from 'discord-api-types/v10'
 import { Colors, colorPalette } from './colors'
 import { Track } from 'discord-player'
+import { log } from 'console'
+
+function truncateText(text: string){
+  let truncate = text.slice(0, 50)  
+  return text.length > 50 ? truncate += '...' : truncate
+}
+
+function addField(name: string, value: string, inline: boolean = false): APIEmbedField{
+  return {
+    name: name,
+    value: truncateText(value),
+    inline
+  }
+}
+
 
 export function embedTrackMessage(
   message: Message,
   color: colorPalette,
-  title: string,
+  text: string,
   { ...props }: Track
 ) {
-  const embed = new EmbedBuilder()
+  const source = props.queryType as string
+  const fields = [
+    addField("Cantor:", props.author),
+    addField("Música:", props.title),
+    addField("Fonte:", source)
+  ]
+
+  try {
+    const embed = new EmbedBuilder()
     .setColor(new Colors()[color])
-    .setTitle(title)
+    .setTitle(text)
     .setURL(props.url)
     .setAuthor({
       name: props.author,
     })
     .setThumbnail(props.thumbnail)
+    .setFields(fields)
     .setTimestamp()
     .setFooter({
-      text: props.duration,
-      iconURL: '⏳',
+      text: `⏳ Duração: ${props.duration} ⏳`,
     })
 
   message.channel.send({ embeds: [embed] })
+  } catch(ex) {
+    log(ex)
+  }
 }

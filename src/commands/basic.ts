1318aee2f3a0f5056bb7c29a88149dd035d99070
AiramVieira <leonardo.airam12@gmail.com>
2023-08-07 22:59:03 -0300

    folder organization

--- src/commands/basic.ts
+++
@@ -1,8 +0,0 @@
-import { userInfo } from "os"
-import { addCommand, Command } from "../utils/commands"
-import { ping } from "./basic/commands/ping"
-
-export const basicCommandsList: Array<Command[]> = [
-    addCommand(["ping"], ping),
-    addCommand(["user", "profile"], userInfo)
-]
